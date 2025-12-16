5e92f6f34e52b19566e8bf34d652935db7beb1ee
Ef <phurin.ef@gmail.com>
2025-12-16 16:17:48 +0700

    lab8_3 complete

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -11,3 +11,53 @@
 "Brook"
 "Jinbe"
 */
+
+#include<iostream>
+#include<iomanip>
+#include<string>
+using namespace std;
+
+int main(){
+    int age = 0;
+    int height = 0;
+    int bounty = 0;
+    cout << "Enter your age: ";
+    cin >> age;
+    if (age <= 25){
+        cout << "Enter your height: ";
+        cin >> height;
+        if (height < 100){
+            cout << "Your character = Chopper";
+        }
+        else if(height < 180){
+            cout << "Your character = Usopp";
+        }
+        else{
+            cout <<"Enter your bounty: ";
+            cin >> bounty;
+            if(bounty > 1100000000){
+                cout << "Your character = Zoro";
+            }
+            else{
+                cout << "Your character = Sanji";
+            }
+            return 0;
+    }
+    }
+    else if (age <= 60){
+        cout <<"Enter your bounty: ";
+        cin >> bounty;
+        if(bounty > 500000000){
+                cout << "Your character = Jinbe";
+            }
+        else{
+                cout << "Your character = Franky";
+            }
+            return 0;
+    }
+    else if(age >60){
+        cout << "Your character = Brook";
+        return 0;
+    }
+    
+}
